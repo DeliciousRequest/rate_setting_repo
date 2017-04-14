ba14e4680d112471d5c9b206d14a808fb477f4cc
DeliciousRequest <alex.lange9@gmail.com>
2017-04-14 08:16:37 -0600

    Figured out how to completely handle files!

--- rate_setting_project/rate_setting_app/urls.py
+++ rate_setting_project/rate_setting_app/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
    url(r'^$', views.validation, name='validation'), 
    url(r'^tables/$', views.tables, name='tables'),
-   url(r'^validate_tables/$', views.validate_tables, name='validate_tables'),
 ]
 
 if settings.DEBUG:
--- rate_setting_project/rate_setting_app/views.py
+++ rate_setting_project/rate_setting_app/views.py
@@ -2,33 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.template.context import RequestContext
-import openpyxl
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+
 import openpyxl
 
 # Create your views here.
-#def validation(request):
- #   return render(request, 'rate_setting_app/validation.html')
     
-def tables(request):
-    return render(request, 'rate_setting_app/tables.html')
-
-def validate_tables(request):
-    error_message = 'Validation will go here.'
-    return HttpResponse(error_message)
-
 def validation(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
-        #wb = openpyxl.load_workbook(fs.path)
-        #sheet = wb.active
-        table_name = 'test table text' #sheet['A1']
+        wb = openpyxl.load_workbook(request.FILES['myfile'])
+        sheet = wb.active
+        table_name = sheet['A2'].value + '\n test'
         return render(request, 'rate_setting_app/validation.html', {
             'uploaded_file_url': uploaded_file_url,
             'table_name': table_name
         })
     return render(request, 'rate_setting_app/validation.html')
+    
+def tables(request):
+    return render(request, 'rate_setting_app/tables.html')
