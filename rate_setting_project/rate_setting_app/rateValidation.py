d69479b6593b35bfc8c20bacf6db540bc8b5e7e1
DeliciousRequest <alex.lange9@gmail.com>
2017-05-02 17:50:57 -0600

    Completely unit tested and db2 functionality added.

--- rate_setting_project/rate_setting_app/rateValidation.py
+++ rate_setting_project/rate_setting_app/rateValidation.py
@@ -1,15 +1,66 @@
 #rateValidation.py - Checks if a rate input sheet is valid.
 
 #Modules
-import openpyxl, datetime, re
+import openpyxl, datetime, re, ibm_db
 
-#Global Variables
+#Global Constants
 validStates = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS',
                'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC',
                'ND', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI',
                'WY', 'OT', '***']
 
+conn = ibm_db.connect('DATABASE=DSNDP30;HOSTNAME=DSNDP30;PORT=2646;PROTOCOL=TCPIP;UID=E0015EY;PWD=acTrpj83;', '', '')
+sqlT024X1 = 'SELECT DISTINCT DCLR_INT_RT_TBL FROM E0015DB.T024X WHERE COMPANY_CODE = \'MLF\''
+sqlT024X2 = 'SELECT DISTINCT GUAR_INT_RT_TBL FROM E0015DB.T024X WHERE COMPANY_CODE = \'MLF\''
+sqlTAD3F = 'SELECT DISTINCT DECL_TBL_NUMBER FROM E0015DB.TAD3F WHERE COMPANY_CODE = \'MLF\''
+sqlT0C8X1 = 'SELECT DISTINCT CAP_RATE_TBL FROM E0015DB.T0C8X WHERE COMPANY_CODE = \'MLF\''
+sqlT0C8X2 = 'SELECT DISTINCT TRIGGER_RATE_TBL FROM E0015DB.T0C8X WHERE COMPANY_CODE = \'MLF\''
+sqlT0C8X3 = 'SELECT DISTINCT GUAR_CAP_RATE_TBL FROM E0015DB.T0C8X WHERE COMPANY_CODE = \'MLF\''
+sqlT0C8X4 = 'SELECT DISTINCT GUAR_TRIG_RATE_TBL FROM E0015DB.T0C8X WHERE COMPANY_CODE = \'MLF\''
+sqlT0C8X5 = 'SELECT DISTINCT GUAR_SPREAD_TBL FROM E0015DB.T0C8X WHERE COMPANY_CODE = \'MLF\''
+field24X1 = 'DCLR_INT_RT_TBL'
+field24X2 = 'GUAR_INT_RT_TBL'
+fieldD3F = 'DECL_TBL_NUMBER'
+fieldT0C8X1 = 'CAP_RATE_TBL'
+fieldT0C8X2 = 'TRIGGER_RATE_TBL'
+fieldT0C8X3 = 'GUAR_CAP_RATE_TBL'
+fieldT0C8X4 = 'GUAR_TRIG_RATE_TBL'
+fieldT0C8X5 = 'GUAR_SPREAD_TBL'
+
+#Global Variables
+subsetT024X1 = []
+subsetT024X2 = []
+subsetTAD3F = []
+subsetT0C8X1 = []
+subsetT0C8X2 = []
+subsetT0C8X3 = []
+subsetT0C8X4 = []
+subsetT0C8X5 = []
+
 #Functions
+def populateSubsets(inputConn, inputSQL, inputSubset, inputField):
+    stmt = ibm_db.exec_immediate(inputConn, inputSQL)
+    dictionary = ibm_db.fetch_assoc(stmt)
+    while dictionary != False:
+        inputSubset.append(str(dictionary[inputField]).rstrip())
+        dictionary = ibm_db.fetch_assoc(stmt)
+
+def validateTableSubset(inputCellValue, inputRow, inputSubset, inputSubset2 = None, inputSubset3 = None, inputSubset4 = None):
+    subsetError = []
+    if len(str(inputCellValue)) > 16:
+        subsetError.append('The TABLE_SUBSET record is too long. Row ' + str(inputRow) + '.')
+    else:
+        if str(inputCellValue) not in inputSubset:
+            if inputSubset2 != None and str(inputCellValue) not in inputSubset2:
+                if inputSubset3 != None and str(inputCellValue) not in inputSubset3:
+                    if inputSubset4 != None and str(inputCellValue) not in inputSubset4:
+                        subsetError.append('The TABLE_SUBSET record is invalid. Row ' + str(inputRow) + '.')
+                else:
+                    subsetError.append('The TABLE_SUBSET record is invalid. Row ' + str(inputRow) + '.')
+            else:
+                subsetError.append('The TABLE_SUBSET record is invalid. Row ' + str(inputRow) + '.')
+    return subsetError
+
 def printErrorMessages(errorList):
     errorListOutput = ''
     
@@ -23,8 +74,8 @@ def validateControlRecord(inputSheet):
     if inputSheet['A2'].value != inputSheet.title: #Check TABLE_NAME
         controlRecordErrors.append('The TABLE_NAME record is invalid. Cell A2.')
 
-    if inputSheet['B2'].number_format != 'mm/dd/yyyy': #DATE format check
-        controlRecordErrors.append('The DATE record is not correctly formatted. Cell B2.')
+    #if inputSheet['B2'].number_format != 'mm/dd/yyyy': #DATE format check
+        #controlRecordErrors.append('The DATE record is not correctly formatted. Cell B2.')
 
     try: #Validate cycle date
         cycleDate = str(inputSheet['B2'].value)
@@ -72,41 +123,52 @@ def validateData(inputSheet):
     allAddRows = []
     rowData = []
     
-    for column in range(1,inputSheet.max_column + 1): #begin validating data, column by column
-            if inputSheet.max_row == 3: #Check to see if data is present
-                dataErrors.append('No data is present in the spreadsheet.')
-                break
-            for row in range(4, inputSheet.max_row + 1):
-                if inputSheet.cell(row = row, column = column).value == None: #Blank cell check
+    if inputSheet.max_row == 3: #Check to see if data is present, should be outside of for loop to reduce number of checks in loop
+        dataErrors.append('No data is present in the spreadsheet.')
+    elif inputSheet.title == 'T025X' and inputSheet.max_column != 9:
+        dataErrors.append('An incorrect number of columns are present in the spreadsheet. Further validation cannot occur.')
+    elif inputSheet.title == 'T026X' and inputSheet.max_column != 9:
+        dataErrors.append('An incorrect number of columns are present in the spreadsheet. Further validation cannot occur.')
+    elif inputSheet.title == 'TU130' and inputSheet.max_column != 6:
+        dataErrors.append('An incorrect number of columns are present in the spreadsheet. Further validation cannot occur.')
+    else:
+        for row in range(4,inputSheet.max_row + 1): #begin validating data, row by row   
+            for column in range(1, inputSheet.max_column + 1):
+                currentCellValue = inputSheet.cell(row = row, column = column).value
+                if currentCellValue == None: #Blank cell check
                     dataErrors.append('A blank cell is present. Row ' + str(row) + '.')
                     continue
                 if inputSheet.title == 'T025X':
+                    populateSubsets(conn, sqlT024X1, subsetT024X1, field24X1)
+                    populateSubsets(conn, sqlTAD3F, subsetTAD3F, fieldD3F)
+                    populateSubsets(conn, sqlT0C8X1, subsetT0C8X1, fieldT0C8X1)
+                    populateSubsets(conn, sqlT0C8X2, subsetT0C8X2, fieldT0C8X2)
+                    
                     if column == 1: #COMPANY_CODE check
                         dataErrors.extend(validateCompanyCode(inputSheet, row, column))
                     if column == 2: #PRODUCT_PREFIX check
                         dataErrors.extend(validateProductPrefix(inputSheet, row, column))
                     if column == 3: #TABLE_SUBSET check
-                        if len(str(inputSheet.cell(row = row, column = column).value)) > 16:
-                            dataErrors.append('The TABLE_SUBSET record is too long. Row ' + str(row) + '.')
+                        dataErrors.extend(validateTableSubset(currentCellValue, row, subsetT024X1, subsetTAD3F, subsetT0C8X1, subsetT0C8X2))
                     if column == 4: #ISSUE_STATE check
                         dataErrors.extend(validateIssueState(inputSheet, row, column))
                     if column == 5: #RCPT_PERD_STRT_DT check
                         try:
                              startDate = str(inputSheet.cell(row = row, column = column).value)
-                             datetime.datetime.strptime(startDate[0:10], '%Y-%m-%d')
+                             datetime.datetime.strptime(startDate, '%Y-%m-%d')
                         except:
                              dataErrors.append('The RCPT_PERD_STRT_DT record is invalid. Row ' + str(row) + '.')
                     if column == 6: #INT_RT_EFF_DT check
                         try:
                             effectiveDate = str(inputSheet.cell(row = row, column = column).value)
-                            datetime.datetime.strptime(effectiveDate[0:10], '%Y-%m-%d')
+                            datetime.datetime.strptime(effectiveDate, '%Y-%m-%d')
                         except:
-                            dataErrors.append('The RCPT_PERD_STRT_DT record is invalid. Row ' + str(row) + '.')
+                            dataErrors.append('The INT_RT_EFF_DT record is invalid. Row ' + str(row) + '.')
                     if column == 7: #SETTL_DATE_IND check
                         if len(str(inputSheet.cell(row = row, column = column).value)) > 1:
                             dataErrors.append('The SETTL_DATE_IND record is too long. Row ' + str(row) + '.')
                     if column == 8: #INTEREST_RATE check
-                        if re.search(r'^\d{1,2}\.\d{0,3}$', str(inputSheet.cell(row = row, column = column).value)) == None:
+                        if re.search(r'^\d{1,2}(\.\d{0,3})?$', str(inputSheet.cell(row = row, column = column).value)) == None:
                             dataErrors.append('An invalidly formatted INTEREST_RATE record is present. Row ' + str(row) + '.')
                         elif float(inputSheet.cell(row = row, column = column).value) < 0 or float(inputSheet.cell(row = row, column = column).value) > 99.999:
                             dataErrors.append('An INTEREST_RATE is outside of the acceptable threshold. Row ' + str(row) + '.')
@@ -115,7 +177,7 @@ def validateData(inputSheet):
                             dataErrors.append('The ACTION record is invalid. Row ' + str(row) + '.')
 
                         if inputSheet.cell(row = row, column = column).value == 'A': #Duplicate add row check
-                            for i in range(0,6):
+                            for i in range(0,8):
                                 rowData.append(inputSheet.cell(row = row, column = i + 1).value)
                             if rowData in allAddRows:
                                 dataErrors.append('A duplicate row is present. Row ' + str(row) + '.')
@@ -123,6 +185,11 @@ def validateData(inputSheet):
                             rowData = []
 
                 elif validateTable(inputSheet) == 'T026X': #Run validation on the inputSheet, based on table selection.
+                    populateSubsets(conn, sqlT024X2, subsetT024X2, field24X2)
+                    populateSubsets(conn, sqlT0C8X3, subsetT0C8X3, fieldT0C8X3)
+                    populateSubsets(conn, sqlT0C8X4, subsetT0C8X4, fieldT0C8X4)
+                    populateSubsets(conn, sqlT0C8X5, subsetT0C8X5, fieldT0C8X5)
+                    
                     if column == 1: #COMPANY_CODE check
                         dataErrors.extend(validateCompanyCode(inputSheet, row, column))
                     if column == 2: #PRODUCT_PREFIX check
@@ -135,7 +202,7 @@ def validateData(inputSheet):
                     if column == 5: #EFFECTIVE_DATE check
                         try:
                             effectiveDate = str(inputSheet.cell(row = row, column = column).value)
-                            datetime.datetime.strptime(effectiveDate[0:10], '%Y-%m-%d')
+                            datetime.datetime.strptime(effectiveDate, '%Y-%m-%d')
                         except:
                             dataErrors.append('The EFFECTIVE_DATE record is invalid. Row ' + str(row) + '.')
                     if column == 6: #MAXIMUM_DURATION check
@@ -164,7 +231,7 @@ def validateData(inputSheet):
                             dataErrors.append('The ACTION record is invalid. Row ' + str(row) + '.')
 
                         if inputSheet.cell(row = row, column = column).value == 'A': #Duplicate add row check
-                            for i in range(0,6):
+                            for i in range(0,8):
                                 rowData.append(inputSheet.cell(row = row, column = i + 1).value)
                             if rowData in allAddRows:
                                 dataErrors.append('A duplicate row is present. Row ' + str(row) + '.')
@@ -180,14 +247,14 @@ def validateData(inputSheet):
                     if column == 3: #EFFECTIVE_DATE check:
                         try:
                             effectiveDate = str(inputSheet.cell(row = row, column = column).value)
-                            datetime.datetime.strptime(effectiveDate[0:10], '%Y-%m-%d')
+                            datetime.datetime.strptime(effectiveDate, '%Y-%m-%d')
                         except:
                             dataErrors.append('The EFFECTIVE_DATE record is invalid. Row ' + str(row) + '.')
                     if column == 4: #GUAR_PERIOD check
                         if re.search(r'^\d{1,4}$', str(inputSheet.cell(row = row, column = column).value)) == None:
                             dataErrors.append('An invalidly formatted GUAR_PERIOD record is present. Row ' + str(row) + '.')
                     if column == 5: #INDEX_RATE checks
-                        if re.search(r'^\.\d{1,5}$', inputSheet.cell(row = row, column = column).value) == None:
+                        if re.search(r'^\.\d{1,5}$', str(inputSheet.cell(row = row, column = column).value)) == None:
                             dataErrors.append('An invalidly formatted INDEX_RATE record is present. Row ' + str(row) + '.')
                         elif float(inputSheet.cell(row = row, column = 5).value) < 0 or float(inputSheet.cell(row = row, column = column).value) > 0.25:
                             dataErrors.append('An INDEX_RATE is outside of the acceptable threshold. Row ' + str(row) + '.')
@@ -195,7 +262,7 @@ def validateData(inputSheet):
                         if inputSheet.cell(row = row, column = column).value not in ['U', 'A']: #valid action check
                             dataErrors.append('The ACTION record is invalid. Row ' + str(row) + '.')
                         if inputSheet.cell(row = row, column = column).value == 'A': #Duplicate add row check
-                            for i in range(0,6):
+                            for i in range(0,5):
                                 rowData.append(inputSheet.cell(row = row, column = i + 1).value)
                             if rowData in allAddRows:
                                 dataErrors.append('A duplicate row is present. Row ' + str(row) + '.')
@@ -224,6 +291,3 @@ def validateSpreadsheet(inputXLSX):
         errorMessageString = printErrorMessages(errorMessages)
         #errorMessageString = ''.join(map(str, errorMessages))
         return(errorMessageString) #Print error messages
-
-#Program
-#print('Please note that this program cannot check date formats or TABLE_SUBSET validity, only date validity and TABLE_SUBSET format.)')
